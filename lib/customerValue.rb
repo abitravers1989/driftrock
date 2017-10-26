febe6e6120d17abebfe284c81d77774d436c7c42
Abi Travers <atravers4007@gmail.com>
2017-10-26 08:57:01 +0100

    Tests for Customer value class

---
+++ lib/customerValue.rb
@@ -0,0 +1,8 @@
+#Class will calculate the highest valuing customer. It will output an email to the user.
+require_relative 'accessingData.rb'
+
+class CustomerValue
+  
+
+
+end
--- spec/customerValue_spec.rb
+++ spec/customerValue_spec.rb
@@ -20,6 +20,7 @@ describe CustomerSpend do
      [{"id":"KZHR-1H35-2IH8-JXYN","first_name":"Quincy","last_name":"Schimmel","phone":"186.301.6921 x948","email":"schimmel_quincy@ernser.io"},
     {"id":"S27G-8UMJ-LDSL-UOPN","first_name":"Henry","last_name":"Terry","phone":"636-387-6074 x690","email":"terry_henry@doyle.io"}
     ]}
+  }
 
     context '#most_loyal' do
       it 'Finds the ID which occurs the most' do
