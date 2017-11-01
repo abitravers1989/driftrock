2a675da8608af4eb6b370dcd4c8463c3885cfbd0
Abi Travers <atravers4007@gmail.com>
2017-11-01 13:12:21 +0000

    figuring out highest spend. Gaining visibility of the method

--- lib/customer_value.rb
+++ lib/customer_value.rb
@@ -1,4 +1,89 @@
 # Class will calculate the highest valuing customer. It will output an email to the user.
+require_relative './lib/customer_spend.rb'
+
+
+purchase_data =
+  [
+    {
+      'user_id' => 'KZHR-1H35-2IH8-JXYN',
+      'item' => 'Synergistic Aluminum Shoes',
+      'spend' => '27.78'
+    },
+    {
+      'user_id' => 'S27G-8UMJ-LDSL-UOPN',
+      'item' => 'Gorgeous Paper Hat',
+      'spend' => '54.5'
+    },
+    {
+      'user_id' => 'KZHR-1H35-2IH8-JXYN',
+      'item' => 'Aerodynamic Copper Bench',
+      'spend' => '43.21'
+    },
+    {
+      'user_id' => 'KZHR-1H35-2IH8-JXYN',
+      'item' => 'Aerodynamic Copper Bench',
+      'spend' => '35.21'
+    }
+  ]
+
+# data.user_url double
+user_data =
+  [
+    {
+      'id' => 'KZHR-1H35-2IH8-JXYN',
+      'first_name' => 'Quincy',
+      'last_name' => 'Schimmel',
+      'phone' => '186.301.6921 x948',
+      'email' => 'schimmel_quincy@ernser.io'
+    },
+    {
+      'id' => 'S27G-8UMJ-LDSL-UOPN',
+      'first_name' => 'Henry',
+      'last_name' => 'Terry',
+      'phone' => '636-387-6074 x690',
+      'email' => 'terry_henry@doyle.io'
+    }
+  ]
+
+# @customer_id = "Nill"
+@highest_spend = 0
+
+user_data.each do |k|
+  var = k["email"]
+  puts "first #{@highest_spend}"
+  customer = CustomerSpend.new(var, user_data, purchase_data)
+  var_2 = customer.calculating_total_spend.to_f
+  puts "second #{@highest_spend}"
+  if (var_2 > @highest_spend)
+    @highest_spend = var_2
+   end
+ end
+ puts "Last hS #{@highest_spend}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 require_relative 'customer_spend.rb'
 
 class CustomerValue(purchase_data, user_data)
@@ -13,22 +98,59 @@ def highest_value(purchase_data)
   highest_spend = 0
     user_data.each do |k|
        var = k["email"]
+       p var
        customer = CustomerSpend.new(var, user_data, purchase_data)
          if (customer.calculating_total_spend.to_f) >= (highest_spend)
-        @customer_email = var
-      end
-     end
- #    email_remeber_highest = 0
- #      users = user_data.map{|user| user["email"]}
- #      users.each do |k |
- #        p k
- #        cus = CustomerSpend.new(k, user_data, purchase_data)
- #        p k
- #        if cus.calculating_total_spend.to_f > email_remeber_highest
- #           p k
- #          @cus_email = k
- #          email_remeber_highest += cus.calculating_total_spend.to_f
- #      end
- #    end
- # end
-end
+           p customer
+           @customer_id = var
+        end
+    end
+  end
+
+
+
+purchase_data =
+  [
+    {
+      'user_id' => 'KZHR-1H35-2IH8-JXYN',
+      'item' => 'Synergistic Aluminum Shoes',
+      'spend' => '27.78'
+    },
+    {
+      'user_id' => 'S27G-8UMJ-LDSL-UOPN',
+      'item' => 'Gorgeous Paper Hat',
+      'spend' => '54.5'
+    },
+    {
+      'user_id' => 'KZHR-1H35-2IH8-JXYN',
+      'item' => 'Aerodynamic Copper Bench',
+      'spend' => '43.21'
+    },
+    {
+      'user_id' => 'KZHR-1H35-2IH8-JXYN',
+      'item' => 'Aerodynamic Copper Bench',
+      'spend' => '35.21'
+    }
+  ]
+
+# data.user_url double
+user_data =
+  [
+    {
+      'id' => 'KZHR-1H35-2IH8-JXYN',
+      'first_name' => 'Quincy',
+      'last_name' => 'Schimmel',
+      'phone' => '186.301.6921 x948',
+      'email' => 'schimmel_quincy@ernser.io'
+    },
+    {
+      'id' => 'S27G-8UMJ-LDSL-UOPN',
+      'first_name' => 'Henry',
+      'last_name' => 'Terry',
+      'phone' => '636-387-6074 x690',
+      'email' => 'terry_henry@doyle.io'
+    }
+  ]
+customer = CustomerValue.new(purchase_data, user_data)
+customer.highest_value
+customer.customer_id
